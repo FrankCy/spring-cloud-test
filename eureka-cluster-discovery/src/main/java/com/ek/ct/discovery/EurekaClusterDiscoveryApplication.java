f0d4ae6b833d0dca9a90bf3c34c19493e4f549d0
cyoung <cy880708@163.com>
2018-06-15 15:20:27 +0800

    模拟客户端访问

--- eureka-cluster-discovery/src/main/java/com/ek/ct/discovery/EurekaClusterDiscoveryApplication.java
+++ eureka-cluster-discovery/src/main/java/com/ek/ct/discovery/EurekaClusterDiscoveryApplication.java
@@ -4,6 +4,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * @description：
+ * @version 1.0
+ * @author: Yang.Chang
+ * @email: cy880708@163.com
+ * @date: 2018/6/15 下午3:19
+ * @mofified By:
+ */
 @SpringBootApplication
 @EnableEurekaClient
 public class EurekaClusterDiscoveryApplication {
